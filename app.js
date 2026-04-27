298be9d9e88c010ec692e463d046ef5d8be933d1
Ajayi Olufemi <olufemiba15b@gmail.com>
2026-04-27 22:02:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to My Production Web App</h1>');
+});
+
+app.get('/health', (req, res) => {
+  res.send('Server is healthy');
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
